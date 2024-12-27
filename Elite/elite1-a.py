c1d60dfffd2072b279a66431dd08a8ec0a6fcfd0
Aditya Chauhan <apple@Apples-MBP.mshome.net>
2024-12-27 12:08:17 +0530

    us

--- Elite/elite1-a.py
+++ Elite/elite1-a.py
@@ -1,2 +1,19 @@
-st=(1,2,3,4,5)
-count=st.count("a",6,len(st))
+# def f1():
+#     print("hello")
+
+# print(f1)
+
+
+# x=5
+# y=5
+# z=5
+# print(id(x), id(y),id(z))
+# z+=1
+# print(id(x), id(y),id(z))
+
+# x1 = {0:1, False:'a', 'b':True,1:{2,3,5,1}}
+# x2={{1,2}:[1,2]}
+# y=[1,2,3,4,5]
+# print(x1)
+# print(type({}))
+ww
